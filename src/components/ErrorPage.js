393b4985bb71e622684b2d9da66525c4d2992e15
Andrew Chacon <andrew@Andrews-MacBook-Pro.local>
2019-05-13 09:02:31 -0400

    error route/page

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function ErrorPage() {
   return (
-    <div>
-      <h1>Error Page Not Found</h1>
+    <div className="error">
+      <h2 className="error__title">Natours</h2>
+      <h1 className="error__message">Sorry that page doesnt exist</h1>
+      <br />
+      <Link className="error__link" to="/">
+        <p>Go back to the home page.</p>
+      </Link>
     </div>
   );
 }
