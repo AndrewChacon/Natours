d5c3307d45f806ba5982ccb67c1dd77c82f0c32e
Andrew Chacon <andrew@Andrews-MacBook-Pro.local>
2019-05-14 20:33:49 -0400

    changes

---
+++ src/components/Pages/SuccessPage/SuccessPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class SuccessPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Success Page</h1>
+      </div>
+    );
+  }
+}
