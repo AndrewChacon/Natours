2f752abc35755326a04d66a969ab3a4de2526be9
Andrew Chacon <andrew@Andrews-MacBook-Pro.local>
2019-05-15 20:26:36 -0400

    navbar toggle

--- src/components/Pages/HomePage/MainBody/Features/Features.js
+++ src/components/Pages/HomePage/MainBody/Features/Features.js
@@ -1,57 +1,34 @@
 import React from "react";
+import FeatureCard from "./FeatureCard/FeatureCard";
 
 const Features = () => {
   return (
     <section className="section-features" id="benefits">
       <div className="row">
-        <div className="col-1-of-4">
-          <div className="feature-box">
-            <i className="feature-box__icon icon-basic-world" />
-            <h3 className="heading-tertiary u-margin-bottom-small">
-              Explore the world
-            </h3>
-            <p className="feature-box__text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores,
-              rem.
-            </p>
-          </div>
-        </div>
-        <div className="col-1-of-4">
-          <div className="feature-box">
-            <i className="feature-box__icon icon-basic-compass" />
-            <h3 className="heading-tertiary u-margin-bottom-small">
-              Meet nature
-            </h3>
-            <p className="feature-box__text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores,
-              rem.
-            </p>
-          </div>
-        </div>
-        <div className="col-1-of-4">
-          <div className="feature-box">
-            <i className="feature-box__icon icon-basic-map" />
-            <h3 className="heading-tertiary u-margin-bottom-small">
-              Find your way
-            </h3>
-            <p className="feature-box__text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores,
-              rem.
-            </p>
-          </div>
-        </div>
-        <div className="col-1-of-4">
-          <div className="feature-box">
-            <i className="feature-box__icon icon-basic-heart" />
-            <h3 className="heading-tertiary u-margin-bottom-small">
-              Live a healthier life
-            </h3>
-            <p className="feature-box__text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores,
-              rem.
-            </p>
-          </div>
-        </div>
+        <FeatureCard
+          icon="icon-basic-world"
+          title="explore the world"
+          text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores,
+          rem."
+        />
+        <FeatureCard
+          icon="icon-basic-compass"
+          title="Meet nature"
+          text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores,
+          rem."
+        />
+        <FeatureCard
+          icon="icon-basic-map"
+          title="Find your way"
+          text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores,
+          rem."
+        />
+        <FeatureCard
+          icon="icon-basic-heart"
+          title="Live a healthier life"
+          text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores,
+          rem."
+        />
       </div>
     </section>
   );
--- src/components/Pages/HomePage/NavigationBar/NavigationBar.js
+++ src/components/Pages/HomePage/NavigationBar/NavigationBar.js
@@ -1,49 +1,95 @@
-import React from "react";
+import React, { Component } from "react";
 
-const NavigationBar = () => {
-  return (
-    <div className="navigation">
-      <input
-        type="checkbox"
-        name=""
-        id="navi-toggle"
-        className="navigation__checkbox"
-      />
-      <label htmlFor="navi-toggle" className="navigation__button">
-        <span className="navigation__icon">&nbsp;</span>
-      </label>
-      <div className="navigation__background">&nbsp;</div>
-      <nav className="navigation__nav">
-        <ul className="navigation__list">
-          <li className="navigation__item">
-            <a href="#about" className="navigation__link">
-              About Natous
-            </a>
-          </li>
-          <li className="navigation__item">
-            <a href="#benefits" className="navigation__link">
-              Your Benefits
-            </a>
-          </li>
-          <li className="navigation__item">
-            <a href="#section-tours" className="navigation__link">
-              Popular Tours
-            </a>
-          </li>
-          <li className="navigation__item">
-            <a href="#stories" className="navigation__link">
-              Stories
-            </a>
-          </li>
-          <li className="navigation__item">
-            <a href="#book" className="navigation__link">
-              Book Now
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  );
-};
+export default class NavigationBar extends Component {
+  state = {
+    isToggleOn: false
+  };
 
-export default NavigationBar;
+  handleClick = () => {
+    this.setState(state => ({
+      isToggleOn: !state.isToggleOn
+    }));
+    console.log(this.state.isToggleOn);
+  };
+
+  render() {
+    return (
+      <div className="navigation">
+        <input
+          type="checkbox"
+          name=""
+          id="navi-toggle"
+          className="navigation__checkbox"
+          onClick={this.handleClick}
+        />
+        <label htmlFor="navi-toggle" className="navigation__button">
+          <span className="navigation__icon">&nbsp;</span>
+        </label>
+        <div
+          className={
+            this.state.isToggleOn
+              ? "navigation__background"
+              : "navigation__background--hidden"
+          }
+        >
+          &nbsp;
+        </div>
+        <nav
+          className={
+            this.state.isToggleOn
+              ? "navigation__nav"
+              : "navigation__nav--hidden"
+          }
+        >
+          <ul className="navigation__list">
+            <li className="navigation__item">
+              <a
+                href="#about"
+                className="navigation__link"
+                onClick={this.handleClick}
+              >
+                About Natous
+              </a>
+            </li>
+            <li className="navigation__item">
+              <a
+                href="#benefits"
+                className="navigation__link"
+                onClick={this.handleClick}
+              >
+                Your Benefits
+              </a>
+            </li>
+            <li className="navigation__item">
+              <a
+                href="#section-tours"
+                className="navigation__link"
+                onClick={this.handleClick}
+              >
+                Popular Tours
+              </a>
+            </li>
+            <li className="navigation__item">
+              <a
+                href="#stories"
+                className="navigation__link"
+                onClick={this.handleClick}
+              >
+                Stories
+              </a>
+            </li>
+            <li className="navigation__item">
+              <a
+                href="#book"
+                className="navigation__link"
+                onClick={this.handleClick}
+              >
+                Book Now
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    );
+  }
+}
